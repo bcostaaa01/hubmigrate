3ecdf68ad1b5b7b7b0dfcefe28f272c4b639257a
Bruno <14274839-bcostaaa01@users.noreply.gitlab.com>
2023-10-27 14:51:24 +0100

    Remove duplicate files

--- auth.py
+++
@@ -1,6 +0,0 @@
-class Auth:
-    """ Authenticate with HubSpot """
-    def get_token():
-        # get HubSpot private app token from input
-        token = input('Enter your HubSpot private app token: ')
-        return token
