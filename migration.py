9904b3041ed39b4575a5e8f0361e58cc654d98aa
Bruno <14274839-bcostaaa01@users.noreply.gitlab.com>
2023-10-27 14:15:42 +0100

    Add test suit for get_data, refactor pipeline

--- migration.py
+++ migration.py
@@ -1,17 +1,17 @@
 # imports
 import requests
 import json
-from auth import Auth
+from ..library.auth import Auth
 
 
 class Migrate():
     """ Migrate data to HubSpot """
-    def __init__(self, object_type, config, hubspot):
+    def __init__(self, object_type, config, hubspot, test_access_token=None):
         self.config = config
         self.hubspot = hubspot
         self.base_path = 'https://api.hubapi.com/crm/v3/objects/'
         self.path = f"{self.base_path}/{object_type}"
-        self.headers = {"Authorization": f"Bearer {Auth.get_token()}"}
+        self.headers = {"Authorization": f"Bearer { test_access_token }"}
         
     def get_data(self, params):
         # get data from path with params
@@ -46,6 +46,6 @@ class Migrate():
         return data
     
     
-migrate = Migrate('contacts', 'config', 'hubspot')
-migrate_data = migrate.get_data('101')
-print(migrate_data)
+# migrate = Migrate('contacts', 'config', 'hubspot')
+# migrate_data = migrate.get_data('101')
+# print(migrate_data)
