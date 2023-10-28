774308b0edcb68cfa8a079798d93e6ad6d02ef69
Bruno <14274839-bcostaaa01@users.noreply.gitlab.com>
2023-10-28 22:54:50 +0100

    Return access token from .env instead of user input

--- classes/auth.py
+++ classes/auth.py
@@ -1,6 +1,11 @@
+# import .env file
+from decouple import config
+
 class Auth:
     """ Authenticate with HubSpot """
     def get_token():
         # get HubSpot private app token from input
-        token = input('Enter your HubSpot private app token: ')
-        return token
+        # token = input('Enter your HubSpot private app token: ')
+        # return token from .env file
+        access_token = config('ACCESS_TOKEN')
+        return access_token
