a3b4ae4de0337e2c3811a3fbe177b1be43f94905
Bruno <14274839-bcostaaa01@users.noreply.gitlab.com>
2023-10-30 15:17:00 +0000

    Remove duplicated folders

--- classes/auth.py
+++
@@ -1,8 +0,0 @@
-# import .env file
-from decouple import config
-
-class Auth:
-    """ Authenticate with HubSpot """
-    def get_token():
-        access_token = config('ACCESS_TOKEN')
-        return access_token
